ea4c009400714abad96fe38776d89279c2ea247e
steveWangzx <stevezxwang@gmail.com>
2023-04-15 01:18:24 -0400

    Init

---
+++ TestSet_generator.py
@@ -0,0 +1,59 @@
+"""
+    This script contains the process of saving image information (Label, Path) into a csv file
+    For Test set Only.
+
+    Author: Zhixiang Wang
+    Date: April 12, 2023
+"""
+
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+import os
+from IPython.display import Image
+from matplotlib import pyplot as plt
+from matplotlib import image as mpimg
+
+for dirname, _, filenames in os.walk('Revitsone-5classes-test'):
+    for filename in filenames:
+        os.path.join(dirname, filename)
+
+# dir path
+BASE_DIR = 'Revitsone-5classes-test/'
+other_activities = BASE_DIR + 'other_activities/'
+safe_driving = BASE_DIR + 'safe_driving/'
+talking_phone = BASE_DIR + 'talking_phone/'
+texting_phone = BASE_DIR + 'texting_phone/'
+turning = BASE_DIR + 'turning/'
+
+files_in_other_activities = sorted(os.listdir(other_activities))
+files_in_safe_driving = sorted(os.listdir(safe_driving))
+files_in_talking_phone = sorted(os.listdir(talking_phone))
+files_in_texting_phone = sorted(os.listdir(texting_phone))
+files_in_turning = sorted(os.listdir(turning))
+
+other_activity_images = ['other_activities/' + i for i in files_in_other_activities]
+safe_driving_images = ['safe_driving/' + i for i in files_in_safe_driving]
+talking_phone_images = ['talking_phone/' + i for i in files_in_talking_phone]
+texting_phone_images = ['texting_phone/' + i for i in files_in_texting_phone]
+turning_images = ['turning/' + i for i in files_in_turning]
+
+images = other_activity_images + safe_driving_images + talking_phone_images + texting_phone_images + turning_images
+
+df = pd.DataFrame()
+df['column'] = [str(x) for x in images]
+
+df[['activity_type', 'image_id']] = df['column'].str.split('/', expand=True)
+df['path'] = BASE_DIR + df['column']
+df = df.drop(columns='column', axis=0)
+
+print(df.head())
+
+plt.title("Test Image")
+plt.xlabel("X pixel scaling")
+plt.ylabel("Y pixels scaling")
+
+image = mpimg.imread(other_activities + df['image_id'].iloc[0])
+plt.imshow(image)
+plt.show()
+
+df.to_csv('driver_behavior_test.csv', index=True)
---
+++ train.py
@@ -0,0 +1,125 @@
+"""
+    This script contains the process of using PyTorch to train the model based on Res-Net 18
+
+    Author: Zhixiang Wang
+    Date: April 13, 2023
+"""
+
+import pandas as pd
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.models as models
+import PIL.Image
+import random
+
+# define the device for training -- 3050ti laptop GPU in my device
+device = torch.device("cuda")
+
+# Transformations for input images
+transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+# Load Dataset
+df = pd.read_csv("driver_behavior.csv")
+
+# Dictionary for labels
+dic = {
+    'other_activities': 4,
+    'safe_driving': 0,
+    'talking_phone': 1,
+    'texting_phone': 2,
+    'turning': 3
+}
+
+
+class CustomDataset(torch.utils.data.Dataset):
+    def __init__(self, df_class, transform_class, device_class):
+        self.df = df_class
+        self.transform = transform_class
+        self.device = device_class
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, index):
+        row = self.df.iloc[index]
+        image_path = row['path']
+        label = row['activity_type']
+        label = dic[label]
+        try:
+            image = PIL.Image.open(image_path).convert('RGB')
+        except PIL.UnidentifiedImageError:
+            # skip this image if it can't be read
+            return None, None
+        image = self.transform(image)
+        image = image.to(self.device)
+        return image, label
+
+
+dataset = CustomDataset(df, transform, device)
+print(dataset.__len__())
+
+# Create data loaders
+batch_size = 32
+train_size = int(0.8 * dataset.__len__())
+val_size = dataset.__len__() - train_size
+train_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, val_size])
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
+
+# Load the model
+resnet = models.resnet18(weights='DEFAULT')
+num_classes = 5
+resnet.fc = nn.Linear(resnet.fc.in_features, num_classes)
+resnet = resnet.to(device)
+
+# Define the loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+criterion = criterion.to(device)
+
+optimizer = optim.SGD(resnet.parameters(), lr=0.001, momentum=0.9)
+
+# Train the model
+num_epochs = 5
+for epoch in range(num_epochs):
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        images, labels = data
+        if images is None:
+            continue  # skip unreadable image
+        images, labels = images.to(device), labels.to(device)
+        optimizer.zero_grad()
+        outputs = resnet(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if i % 100 == 99:  # print every 100 mini-batches
+            print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 100))
+            running_loss = 0.0
+
+# Validation
+correct = 0
+total = 0
+with torch.no_grad():
+    for images, labels in val_loader:
+        images, labels = images.to(device), labels.to(device)
+        outputs = resnet(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the test images: %d %%' % (100 * correct / total))
+
+# Save the model
+scripted_model = torch.jit.script(resnet)
+scripted_model.save('driver_behavior_model_v1.pt')
